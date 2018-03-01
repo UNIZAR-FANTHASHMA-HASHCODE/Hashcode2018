46f6cfff529e336933e692326ab1dc45d2429f4b
asierhandball <asierhandballer@gmail.com>
2018-03-01 20:36:31 +0100

    Updated, now parser also writes the output

--- parserWriter.py
+++ parserWriter.py
@@ -1,23 +1,30 @@
 #File that parse the input file
 
+
+"""This methods returns two lists:
+	--headers: Contains info about the file
+	--rides: Contains N lists with every ride in the problem
+"""
 def getInput(input_file):
 	f = open(input_file, 'r')
 	input = f.read().split('\n')
 	headers = input[0]
 	grid = input[1:]
 	grid.pop()
-	grids = []
+	rides = []
 	headers = headers.split(' ')
 	i=0
 	for element in grid:
 		aux = element.split(' ')
 		aux.append(i)
-		grids.append(aux)
+		rides.append(aux)
 		i=i+1
-	grids.sort(key=lambda x: x[4])
+	rides.sort(key=lambda x: x[4])
 	f.close()
-	return (headers,grids)
+	return (headers,rides)
 
+"""This methods writes the result according to the problem statement
+"""
 def writeOutput(vehicles):
 	f = open("r",'w')
 	for element in vehicles:
